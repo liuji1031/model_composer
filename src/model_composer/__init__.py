d6c0d3d7e2d33629a9c33cc54efd3440c306c75e
Ji Liu <liuji1031@gmail.com>
2025-05-01 21:33:41 -0400

    fix: remove concat

--- src/model_composer/__init__.py
+++ src/model_composer/__init__.py
@@ -1,10 +1,7 @@
 from model_composer.composer import ComposableModel
 from model_composer.registery import ModuleRegistry 
 
-from model_composer.concat import Concat
-
 __all__ = [
     "ComposableModel",
     "ModuleRegistry",
-    "Concat",
 ]
